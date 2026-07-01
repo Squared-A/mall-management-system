1f0c987ce88e06f1f80fb5089de26ef65f5cf2af
Abenet Erbelo <abeneterbelo@gmail.com>
2026-07-01 07:46:25 +0300

    removed unnecessary files

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,8 +1,7 @@
 export const APP_NAME =
   import.meta.env.VITE_APP_NAME || "Mall Management System";
 
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 export const TOKEN_KEY = "mms_access_token";
 export const REFRESH_TOKEN_KEY = "mms_refresh_token";
